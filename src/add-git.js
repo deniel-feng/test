9d2cc1a8f8fa488578d3e1b34b54148236a15c93
Sai Feng <sai.feng@yizhun-ai.com>
2020-07-24 18:13:23 +0800

    6

--- src/add-git.js
+++ src/add-git.js
@@ -13,7 +13,13 @@ const { writeFile } = require('fs');
 exec(`git log -1 --pretty=format:"time=%cd,commit_message=%s,author=%cn"`, (error, stdout) => {
 	console.log('-----',stdout);
 	if (error) throw error;
-	writeFile('commit.txt', stdout, err => {
+	const obj = {};
+	stdout.split(',').forEach(item => {
+		const data = item.split('=');
+		obj[data[0]] = data[1];
+	});
+	const value = `export default ${JSON.stringify(obj)}`;
+	writeFile('commit.js', value, err => {
 		if (err) throw err;
 	})
 });
