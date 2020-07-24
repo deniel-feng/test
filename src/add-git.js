04178588729d25db06a66dc3dec07dedb5c3d950
Sai Feng <sai.feng@yizhun-ai.com>
2020-07-24 17:15:19 +0800

    555

--- src/add-git.js
+++ src/add-git.js
@@ -2,7 +2,13 @@
   * @Created by 冯赛 on 2020/7/24 0024 14:21
 */
 const { exec } = require('child_process');
+const { writeFile } = require('fs');
 
 exec(`git log -1 --pretty=format:"{time：%cd, commit-msg: %s, auther: %cn}"`, (error, stdout, stderr) => {
 	console.log(stdout);
+	if (error) throw error;
+	writeFile('commit.txt', stdout, err => {
+		if (err) throw err;
+	})
+
 });
