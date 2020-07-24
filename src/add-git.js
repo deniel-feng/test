93f383fad8598adcb0fff436833480622d705084
Sai Feng <sai.feng@yizhun-ai.com>
2020-07-24 17:54:11 +0800

    4

--- src/add-git.js
+++ src/add-git.js
@@ -1,11 +1,22 @@
 /*
   * @Created by 冯赛 on 2020/7/24 0024 14:21
 */
+import Vue from "vue/types/vue";
 const { exec } = require('child_process');
 const { writeFile } = require('fs');
 
+Vue.config.productionTip = false
+
+
+// console.log(readFile);
+// readFile('./commit.txt', (err, data) => {
+// 	if (err) throw err;
+// 	console.log(data);
+// })
+
 exec(`git log -1 --pretty=format:"{time：%cd, commit-msg: %s, auther: %cn}"`, (error, stdout) => {
 	console.log(stdout);
+	console.log('window', window);
 	if (error) throw error;
 	writeFile('commit.txt', stdout, err => {
 		if (err) throw err;
--- src/main.js
+++ src/main.js
@@ -2,8 +2,6 @@ import Vue from 'vue'
 import App from './App.vue'
 import 'view-design/dist/styles/iview.css';
 
-Vue.config.productionTip = false
-
 new Vue({
   render: h => h(App),
 }).$mount('#app')
